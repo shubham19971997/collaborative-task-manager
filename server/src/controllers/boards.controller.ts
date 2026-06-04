ec5479e05fc1b54a20e6ff3d63255d3fc1e91391
shubham sikarwar <shubhamsikarwar43@gmail.com>
2026-06-04 23:51:41 +0530

    changes for board apis

--- server/src/controllers/boards.controller.ts
+++ server/src/controllers/boards.controller.ts
@@ -1,4 +1,22 @@
 import { Request, Response } from "express";
 
-export const getBoards = (req:Request, res:Response)=>{};
-export const deleteBoard = (req:Request, res:Response)=>{};
+export const getBoards = (req:Request, res:Response)=>{
+    try{
+
+    
+      } catch (error: any) {
+        res.status(400).json({
+          message: error.message,
+        });
+      }
+};
+export const deleteBoard = (req:Request, res:Response)=>{
+    try{
+
+    
+    } catch (error: any) {
+      res.status(400).json({
+        message: error.message,
+      });
+    }
+};
